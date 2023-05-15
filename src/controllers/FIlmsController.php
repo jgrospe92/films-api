1a1261964cac49d2666fd57b4173b45e8487a494
Jeffrey <jeffreygrospe@yahoo.com>
2023-05-15 15:43:30 -0400

    Update FIlmsController.php
    - remove validation for empty filters

--- src/controllers/FIlmsController.php
+++ src/controllers/FIlmsController.php
@@ -98,9 +98,10 @@ class FilmsController extends BaseController
 
             if (!$this->validateParams($key)) {
                throw new HttpUnprocessableContent($request, "Invalid query parameter : " . "{" . $key . "}");
-            } elseif (empty($value)) {
-               throw new HttpUnprocessableContent($request, "Please provide query value for : " . "{" . $key . "}");
             }
+            // elseif (empty($value)) {
+            //    throw new HttpUnprocessableContent($request, "Please provide query value for : " . "{" . $key . "}");
+            // }
          }
       }
 
