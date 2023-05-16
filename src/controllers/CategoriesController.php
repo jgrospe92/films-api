11981a1ccac1d03c21446c5eb07058a3a49f16d5
Jeffrey <jeffreygrospe@yahoo.com>
2023-05-15 21:49:27 -0400

    Update CategoriesController.php
    - remove validation for empty filters

--- src/controllers/CategoriesController.php
+++ src/controllers/CategoriesController.php
@@ -63,9 +63,10 @@ class CategoriesController extends BaseController
             foreach ($filters as $key => $value) {
                 if (!$this->validateParams($key)) {
                     throw new HttpUnprocessableContent($request, "Invalid query parameter : " . "{" . $key . "}");
-                } elseif (empty($value)) {
-                    throw new HttpUnprocessableContent($request, "Please provide query value for : " . "{" . $key . "}");
                 }
+                // elseif (empty($value)) {
+                //     throw new HttpUnprocessableContent($request, "Please provide query value for : " . "{" . $key . "}");
+                // }
             }
         }
         // verify if client added a page and pageSize params
